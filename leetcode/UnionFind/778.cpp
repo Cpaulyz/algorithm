c8cdd15a5c2e970e2d072e96bb74de265637422c
ChenYZ <181250015@smail.nju.edu.cn>
2021-01-30 22:17:33 +0800

    feat: +778 并查集

---
+++ leetcode/UnionFind/778.cpp
@@ -0,0 +1,79 @@
+// https://leetcode-cn.com/problems/swim-in-rising-water/
+// Created by admin on 2021/1/30.
+// 和778类似，直接并查集完事
+
+using namespace std;
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+class UF {
+public:
+    vector<int> fa;
+    vector<int> sz;
+    int n;
+    int comp_cnt;
+
+public:
+    UF(int _n): n(_n), comp_cnt(_n), fa(_n), sz(_n, 1) {
+        iota(fa.begin(), fa.end(), 0);
+    }
+
+    int findset(int x) {
+        return fa[x] == x ? x : fa[x] = findset(fa[x]);
+    }
+
+    void unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return;
+        }
+        if (sz[x] < sz[y]) {
+            swap(x, y);
+        }
+        fa[y] = x;
+        sz[x] += sz[y];
+        --comp_cnt;
+    }
+
+    bool connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
+};
+
+struct Edge{
+    int from,to,val;
+    Edge(int _from,int _to,int _val):from(_from),to(_to),val(_val){}
+};
+
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int h = grid.size();
+        int w = grid[0].size();
+        UF uf(h*w);
+        vector<Edge> edges;
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < w; ++j) {
+                int id = i*w+j;
+                if(i>0){
+                    edges.emplace_back(id-w,id,max(grid[i-1][j],grid[i][j]));
+                }
+                if(j>0){
+                    edges.emplace_back(id-1,id,max(grid[i][j-1],grid[i][j]));
+                }
+            }
+        }
+        sort(edges.begin(),edges.end(),[](Edge x,Edge y){return x.val<y.val;});
+        for(Edge edge:edges){
+            uf.unite(edge.from,edge.to);
+            if(uf.connected(0,h*w-1)){
+                return edge.val;
+            }
+        }
+        return 0;
+    }
+};
